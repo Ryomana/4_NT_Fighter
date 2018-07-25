5885946fba4d46d789722c55615f1db3f1c02be7
Kirsch Marc <KirschMarc@hotmail.com>
2018-07-25 08:52:27 +0200

    Add a Ship Add a Splash screen Add a continusly playing background music from the splash screen (Injecting into the game loop)

---
+++ Assets/SplashLevelLoad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashLevelLoad : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadFirstLevel", 3f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}

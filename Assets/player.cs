2b8060aabdd29901550efa6deb6116665d31b430
Kirsch Marc <KirschMarc@hotmail.com>
2018-07-26 10:02:43 +0200

    Adding y movements and rotation of the ship

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class player : MonoBehaviour {
 
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 20f;
-    [SerializeField] float xRange = 6f;
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 20f;
+    [SerializeField] float positionPitchFactor = 5f;
+    [SerializeField] float controlPitchFactor = 30f;
+    [SerializeField] float positionYawFactor = 6.5f;
+    [SerializeField] float controlRollFactor = -30f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 2.5f;
+
+    float xThrow, yThrow;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +23,29 @@ public class player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = Time.deltaTime * xThrow * xSpeed;
-        float rawXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange); // transform.localPosition.x + xOffset is the new raw data, it will be limited by xRange
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
-        transform.localPosition = new Vector3(rawXPos, transform.localPosition.y, transform.localPosition.z);
-	}
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float xOffset = Time.deltaTime * xThrow * xSpeed;
+        float yOffset = Time.deltaTime * yThrow * ySpeed;
+        float rawXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange); // transform.localPosition.x + xOffset is the new raw data, it will be limited by xRange
+        float rawYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange);
+
+        transform.localPosition = new Vector3(rawXPos, rawYPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(-pitch, yaw, roll);
+    }
 }
